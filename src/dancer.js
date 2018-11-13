c40eddd67f8b668d411e458ce2851ac5a5a73457
Hack Reactor Student <student@hackreactor.com>
2018-11-12 21:38:28 -0800

    Change directions when hitting a certain point

--- src/dancer.js
+++ src/dancer.js
@@ -40,7 +40,8 @@ var makeDancer = function(top, left, timeBetweenSteps) {
   // this.top = top;
   // this.left = left;
   this.timeBetweenSteps = timeBetweenSteps;
-  this.step();   
+  this.step();
+  this.left;   
   // now that we have defined the dancer object, we can start setting up important parts of it by calling the methods we wrote
   // this one sets the position to some random default point within the body
   this.setPosition(top, left);
@@ -54,6 +55,13 @@ makeDancer.prototype.step = function() {
   // this.step.bind(this);
 
   setTimeout( this.step.bind(this), this.timeBetweenSteps);
+};
+makeDancer.prototype.left = function() {
+  // the basic dancer doesn't do anything interesting at all on each step,
+  // it just schedules the next step
+  // this.step.bind(this);
+
+  setTimeout( this.left.bind(this), this.timeBetweenSteps);
 };
 
 makeDancer.prototype.setPosition = function(top, left) {
--- src/gifDancer.js
+++ src/gifDancer.js
@@ -2,10 +2,11 @@ var makeGifDancer = function(top, left, timeBetweenSteps) {
   
   makeDancer.call(this, top, left, 10);
   this.$node = $('<span class="dancer4"></span>');
-  this.setPosition(top, left)
+  this.setPosition(top, left);
   this.step();
+  this.left;
   this.$node.fadeIn(1000);
-  this.pos = 0;
+  this.pos = Math.floor(Math.random() * 1600);
 };
 
 makeGifDancer.prototype = Object.create(makeDancer.prototype);
@@ -14,14 +15,30 @@ makeGifDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
   // this.$node.fadeIn(5000);
   
-    if(this.pos === 1600){
-      return;
-    }
-    // this.$node.left = this.pos + 'px';
-    this.setPosition(top, this.pos)
-    this.pos += 1;
+  if (this.pos === 1600) {
+    makeDancer.prototype.left.call(this)
+  }else {
+  // this.$node.left = this.pos + 'px';
+  this.setPosition(top, this.pos);
+  this.pos ++ ; 
+  } 
     
-    //makeDancer.prototype.step.call(this) 
+  //makeDancer.prototype.step.call(this) 
   
+};
+makeGifDancer.prototype.left = function() {
+  makeDancer.prototype.left.call(this);
+  // this.$node.fadeIn(5000);
+  if (this.pos === 0) {
+    makeDancer.prototype.step.call(this)
+  } else{
     
+    this.setPosition(top, this.pos);
+    this.pos -- ;
+    
+  }
+  // this.$node.left = this.pos + 'px';
+
+  //makeDancer.prototype.step.call(this) 
+  
 };
