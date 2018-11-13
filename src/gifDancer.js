4c905b02c2744a95e6f03b6eeaea7ae81daff1b7
Hack Reactor Student <student@hackreactor.com>
2018-11-12 21:08:54 -0800

    Added moving dancer

--- src/gifDancer.js
+++ src/gifDancer.js
@@ -1,13 +1,27 @@
 var makeGifDancer = function(top, left, timeBetweenSteps) {
-  makeDancer.call(this, top, left, timeBetweenSteps);
+  
+  makeDancer.call(this, top, left, 10);
   this.$node = $('<span class="dancer4"></span>');
-  this.setPosition(top, left);
+  this.setPosition(top, left)
   this.step();
+  this.$node.fadeIn(1000);
+  this.pos = 0;
 };
 
 makeGifDancer.prototype = Object.create(makeDancer.prototype);
 makeGifDancer.prototype.constructor = makeGifDancer;
 makeGifDancer.prototype.step = function() {
   makeDancer.prototype.step.call(this);
-  this.$node.fadeIn(5000);
+  // this.$node.fadeIn(5000);
+  
+    if(this.pos === 1600){
+      return;
+    }
+    // this.$node.left = this.pos + 'px';
+    this.setPosition(top, this.pos)
+    this.pos += 1;
+    
+    //makeDancer.prototype.step.call(this) 
+  
+    
 };
